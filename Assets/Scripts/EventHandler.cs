9e418feaa11b00b6c4790827147fdcbb3e0777f5
Aditya Sinha <aditya.s@codewalla.com>
2020-02-05 18:35:52 +0530

    XR Interaction toolkit and XR management imported successfully with package manager. Basic XR scene setup with an interactable cube object. Basic scene and scripts created to demonstrate various kinds of event transmission and handling

---
+++ Assets/Scripts/EventHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHandler : MonoBehaviour
+{
+    UnityEventExample refereceUnityEventExample;
+
+    private void Awake()
+    {
+        refereceUnityEventExample = FindObjectOfType<UnityEventExample>();
+
+        refereceUnityEventExample.testEvent.AddListener(UnityEventHandlerSetWithScript);
+
+        refereceUnityEventExample.testEvent.AddListener((string message) =>
+        {
+            Debug.Log("Unity event message added via inline lambda function:" + message);
+        });
+    }
+
+    private void OnEnable()
+    {
+        refereceUnityEventExample.GenericEventExample += EventHandler_GenericEventExample;
+
+        refereceUnityEventExample.ActionEventExample += EventHandler_ActionEventExample;
+    }
+
+    private void EventHandler_ActionEventExample(int arg1, string arg2)
+    {
+        Debug.Log("The Best footballer in the world is:" + arg1.ToString() + arg2);
+    }
+
+    private int EventHandler_GenericEventExample(int a, string s)
+    {
+        Debug.Log("The best footballer in the world is:" + s + a.ToString());
+        return a;
+    }
+
+    public void UnityEventHandler(string message) //this method is set via the inspector for our unity event
+    {
+        Debug.Log("Dynamic unity event message from listener added via inspector:" + message);
+    }
+
+    private void UnityEventHandlerSetWithScript(string message)
+    {
+        Debug.Log("Dynamic unity event message from listener added via script:" + message);
+    }
+
+    private void OnDisable()
+    {
+        refereceUnityEventExample.GenericEventExample -= EventHandler_GenericEventExample;
+
+        refereceUnityEventExample.ActionEventExample -= EventHandler_ActionEventExample;
+    }
+
+
+}
---
+++ Assets/Scripts/UnityEventExample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEventExample : MonoBehaviour
+{
+    [System.Serializable]
+    public class TestEvent : UnityEvent<string> { }
+    public TestEvent testEvent;
+
+    public delegate int DelegateExample(int a, string s);
+    public event DelegateExample GenericEventExample;
+
+    public event Action<int, string> ActionEventExample; //using System library is needed for Action type events
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            testEvent.Invoke("Unity Event Message");
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (GenericEventExample != null)
+            {
+                GenericEventExample(7, "Cristiano Ronaldo");
+            }
+            else
+            {
+                Debug.Log("No listeners found for out event");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (ActionEventExample != null)
+            {
+                ActionEventExample(10, "Leo Messi");
+            }
+            else
+            {
+                Debug.Log("No listeners found for this event");
+            }
+        }
+    }
+}
