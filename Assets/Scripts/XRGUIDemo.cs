12ccfb0575b8df3e68503946c71e8b1548b8a2cb
Aditya Sinha <aditya.s@codewalla.com>
2020-02-06 15:12:41 +0530

    Basic world space XRUI implemented using XRInteraction Toolkit. check scene XRInterface

---
+++ Assets/Scripts/XRGUIDemo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XRGUIDemo : MonoBehaviour
+{
+    [SerializeField] private Image background;
+
+    [SerializeField] private Button exampleButton;
+    [SerializeField] private Slider exampleSlider;
+    [SerializeField] private Toggle exampleToggle;
+
+    [SerializeField] private TextMeshProUGUI textField;
+
+    [SerializeField]private float fixedFontSize = 20f;
+
+    private void Awake()
+    {
+        textField.fontSize = fixedFontSize;
+
+        exampleButton.onClick.AddListener(() =>
+        {
+            textField.text = "Generic Same Text for testing";
+        });
+
+        exampleSlider.onValueChanged.AddListener((float value) => {
+            textField.fontSize = fixedFontSize * value;
+        });
+
+        exampleToggle.onValueChanged.AddListener((bool value) =>
+        {
+            Color currentColor = background.color;
+            currentColor.a = value ? 24 : 0;
+            background.color = currentColor;
+        });
+    }
+
+    
+}
